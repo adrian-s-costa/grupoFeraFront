f89c3c8c937af962bd5428b00667b1fa02ca4795
Adrian <adriancosta1215@gmail.com>
2024-05-18 12:40:33 -0400

    intro fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,17 +17,17 @@ export default function Carousel() {
   const pages = [
     {
       id: 1,
-      imageUrl: "https://res.cloudinary.com/dmo7nzytn/image/upload/v1715983918/intro3_o1hr9c.png",
+      imageUrl: <Image className="w-full" src={"https://res.cloudinary.com/dmo7nzytn/image/upload/v1715983918/intro3_o1hr9c.png"} alt={"intro"} width={428} height={545} ></Image>,
       title: 'Conecte-se com pessoas incriveis'
     },
     {
       id: 2,
-      imageUrl: "https://res.cloudinary.com/dmo7nzytn/image/upload/v1715983918/intro2_cr0hhy.png",
+      imageUrl: <Image className="w-full" src={"https://res.cloudinary.com/dmo7nzytn/image/upload/v1715983918/intro2_cr0hhy.png"} alt={"intro"} width={428} height={545} ></Image>,
       title: 'Faça compras inteligentes'
     },
     {
       id: 3,
-      imageUrl: "https://res.cloudinary.com/dmo7nzytn/image/upload/v1715983918/intro1_lfezp0.png",
+      imageUrl: <Image className="w-full" src={ "https://res.cloudinary.com/dmo7nzytn/image/upload/v1715983918/intro1_lfezp0.png"} alt={"intro"} width={428} height={545} ></Image>,
       title: 'E aproveite benefícios exclusivos que só nossos membros tem acesso'
     }
   ]
@@ -35,10 +35,10 @@ export default function Carousel() {
 
   return (
     <div className="h-screen w-full bg-[#202226] relative">
-      <Image className="w-full" src={pages[counter].imageUrl} alt={"intro"} width={428} height={545} ></Image>
+      {pages[counter].imageUrl}
       <p className="text-white text-[1.8rem] font-bold mt-10 ml-5">{pages[counter].title}</p>
       
-      <div className="flex ml-5 gap-2 mt-10">
+      <div className="flex ml-5 gap-2 mt-5">
         <div className={`${counter == 0 ? 'w-14' : 'w-2'} h-2 ${counter == 0 ? 'bg-white' : 'bg-slate-600'} rounded-full`}></div>
         <div className={`${counter == 1 ? 'w-14' : 'w-2'} h-2 ${counter == 1 ? 'bg-white' : 'bg-slate-600'} rounded-full`}></div>
         <div className={`${counter == 2 ? 'w-14' : 'w-2'} h-2 ${counter == 2 ? 'bg-white' : 'bg-slate-600'} rounded-full`}></div>
@@ -46,7 +46,7 @@ export default function Carousel() {
 
       <button onClick={()=>{ handleChange() }} className={`absolute ${counter == 2 ? 'hidden' : null} right-5 bottom-5 w-[3.5rem] h-[3.5rem] bg-white rounded-full flex justify-center items-center`}><IoIosArrowForward className="text-3xl"/></button>
 
-      {counter == 2 ? <div className="absolute bottom-10 w-full h-[2.80rem] flex justify-center px-8"><button onClick={()=>{router.push('/login')}} className="bg-white rounded-3xl w-full">Vamos?</button></div> : null}
+      {counter == 2 ? <div className="absolute bottom-5 w-full h-[2.80rem] flex justify-center px-8"><button onClick={()=>{router.push('/login')}} className="bg-white rounded-3xl w-full">Vamos?</button></div> : null}
     </div>
 
   );
