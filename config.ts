b9021bb567648849646f78eabdd547401251ecce
Adrian <adriancosta1215@gmail.com>
2024-05-20 11:06:56 -0400

    api url changed

--- config.ts
+++ config.ts
@@ -1,3 +1,3 @@
 export const config = {
-  API_URL: "https://a7c3-2804-14c-7582-5093-1946-f751-d5fc-7d65.ngrok-free.app"
+  API_URL: "https://7a87-2804-14c-7582-5093-1946-f751-d5fc-7d65.ngrok-free.app"
 }
