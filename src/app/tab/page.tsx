322ae0e17b15632ad81a1e75590fa974f4e31601
Adrian <adriancosta1215@gmail.com>
2024-10-01 00:30:28 -0400

    testing params

--- src/app/tab/page.tsx
+++ src/app/tab/page.tsx
@@ -21,6 +21,10 @@ export default function HomeTab(){
   const [muted, setMuted] = useState<any>(null);
   const [tabIndex, setTabIndex] = useState(0)
 
+  if (options){
+    setTabIndex(1)
+  }
+
   useEffect(()=>{
 
   }, [])
@@ -28,8 +32,9 @@ export default function HomeTab(){
   const activePage = typeof window !== "undefined" ? window.localStorage.getItem("page") : undefined;
 
   const handleTabsChange = (index: number) => {
-    localStorage.setItem('page', index.toString())
-    setMuted(true)
+    localStorage.setItem('page', index.toString());
+    setTabIndex(index);
+    setMuted(true);
   }
 
   return(
@@ -47,13 +52,13 @@ export default function HomeTab(){
       </TabPanels>
 
       <TabList className="fixed z-10 h-[4.5rem] bg-white bottom-0 w-full justify-around">
-        <Tab className="flex flex-col">{activePage == "0" ? 
+        <Tab className="flex flex-col">{tabIndex == 0 ? 
           <RiHome5Fill className="text-2xl text-black dark:text-black"/> 
           : <RiHome5Line className="text-2xl text-black dark:text-black"/>}
           <p className="text-black text-[0.5rem]">HOME</p>
         </Tab>
-        <Tab className="flex flex-col">{activePage == "1" ? <PiAirplayFill className="text-2xl text-black dark:text-black"/> : <LuAirplay className="text-2xl text-black dark:text-black"/>} <p className="text-black text-[0.5rem]"> STREAMING </p> </Tab>
-        <Tab className="flex flex-col">{activePage == "2" ? <IoPerson className="text-2xl text-black dark:text-black"/> : <IoPersonOutline className="text-2xl text-black dark:text-black"/>} <p className="text-black text-[0.5rem]"> PERFIL </p> </Tab>
+        <Tab className="flex flex-col">{tabIndex == 1 ? <PiAirplayFill className="text-2xl text-black dark:text-black"/> : <LuAirplay className="text-2xl text-black dark:text-black"/>} <p className="text-black text-[0.5rem]"> STREAMING </p> </Tab>
+        <Tab className="flex flex-col">{tabIndex == 2 ? <IoPerson className="text-2xl text-black dark:text-black"/> : <IoPersonOutline className="text-2xl text-black dark:text-black"/>} <p className="text-black text-[0.5rem]"> PERFIL </p> </Tab>
       </TabList>
     </Tabs>
   )
