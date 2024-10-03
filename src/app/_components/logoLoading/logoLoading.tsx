de7e0a5425112290e5b585f1bd34749558a64fc5
Adrian <adriancosta1215@gmail.com>
2024-10-03 12:34:53 -0400

    feraflix

--- src/app/_components/logoLoading/logoLoading.tsx
+++ src/app/_components/logoLoading/logoLoading.tsx
@@ -5,7 +5,7 @@ export default function LogoLoading(){
     return <>
         <div className="animate-[wiggle_1s_ease-in-out_infinite] h-screen w-screen fixed bg-black z-10 relative flex items-center justify-center">
         <Image 
-        className="pb-6"
+        className="pb-8"
         src={'https://res.cloudinary.com/dmo7nzytn/image/upload/v1727832524/n1_1_1_wo6k0x.png'}
         alt="Logo"
         width={300}
